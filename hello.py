f26896751644202acfe5bedcfca5628bbefea399
zhanlonqi <1955234743@qq.com>
2020-09-23 11:51:00 +0800

    some changes

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-import scipy.io as sio//https://blog.csdn.net/qq_43711697/article/details/104175500
+import scipy.io as sio 
+#https://blog.csdn.net/qq_43711697/article/details/104175500
 datafile="C:\\Users\\Administrator\\Desktop\\hello\\data\\yalemat.mat"
 f =sio.loadmat(datafile)
-print(f['fea'].shape)
+print(f[f.keys[0]].shape)
 
 
