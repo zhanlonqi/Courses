0f9a16e86ad2c476cee2f8081113fe49a6903099
zhanlonqi <1955234743@qq.com>
2020-09-23 11:45:01 +0800

    small changes

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-import scipy.io as scio
-datafile="C:\\Users\\Administrator\\Desktop\\hello\\datayalemat.mat"
-data=scio.loadmat(datafile)
+import scipy.io as sio//https://blog.csdn.net/qq_43711697/article/details/104175500
+datafile="C:\\Users\\Administrator\\Desktop\\hello\\data\\yalemat.mat"
+f =sio.loadmat(datafile)
+print(f['fea'].shape)
+
 
