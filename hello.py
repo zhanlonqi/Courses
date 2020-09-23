c9ab6a7e55d6bdefa5f29b125f6a2a83b9e9a44b
zhanlonqi <46118006+zhanlonqi@users.noreply.github.com>
2020-09-23 19:22:29 +0800

    now it can correctly show the image of the faces

--- hello.py
+++ hello.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 #https://blog.csdn.net/qq_43711697/article/details/104175500
 datafile="C:\\work\\python\\hello\\Courses\\data\\yalemat.mat"
 f =sio.loadmat(datafile)
-a=np.array(32*32)
-a=f['yalemat'][0]
+#a=np.array(32*32)
+a=f['yalemat'].T[2]
 print(f.keys())
-a.shape=11,15
+a.shape=32,32
 a=a.T
 print(a)
 plt.imshow(a,cmap="gray")
