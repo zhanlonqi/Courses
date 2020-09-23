39ef9a3e1b28c1074a31732f4eef980387fbc63f
zhanlonqi <46118006+zhanlonqi@users.noreply.github.com>
2020-09-23 19:25:40 +0800

    renew the strecture of the file

--- yalemat/hello.py
+++ yalemat/hello.py
@@ -2,7 +2,7 @@ import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 #https://blog.csdn.net/qq_43711697/article/details/104175500
-datafile="C:\\work\\python\\hello\\Courses\\yalemat\\data\\yalemat.mat"
+datafile="C:\\work\\python\\hello\\Courses\\data\\yalemat.mat"
 f =sio.loadmat(datafile)
 #a=np.array(32*32)
 a=f['yalemat'].T[2]
