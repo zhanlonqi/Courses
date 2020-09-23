4b1c80c0e7d6a1fe0366c2d75b394be3b18b90c8
zhanlonqi <46118006+zhanlonqi@users.noreply.github.com>
2020-09-23 17:07:29 +0800

    now we can load the mat file

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 import scipy.io as sio 
+import numpy as np
+import matplotlib.pyplot as plt
 #https://blog.csdn.net/qq_43711697/article/details/104175500
-datafile="C:\\Users\\Administrator\\Desktop\\hello\\data\\yalemat.mat"
+datafile="C:\\work\\python\\hello\\Courses\\data\\yalemat.mat"
 f =sio.loadmat(datafile)
-print(f[f.keys[0]].shape)
-
+a=np.array(32*32)
+a=f['yalemat'][0]
+print(f.keys())
+a.shape=11,15
+a=a.T
+print(a)
+plt.imshow(a,cmap="gray")
+plt.show()
 
