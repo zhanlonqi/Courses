341c225ec5535c97d241bd04f285c737f18d369a
zhanlonqi <1955234743@qq.com>
2020-10-09 11:44:45 +0800

    make some tiny changes

--- ex02/ex02.py
+++ ex02/ex02.py
@@ -1,1 +1,9 @@
-print("hello")
+import scipy.io as sio 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+path=os.getcwd()
+datafile=path+"\\ex02\\data\\uci_wine.mat"
+print(datafile)
+f =sio.loadmat(datafile)
+print(f.keys())
--- yalemat/hello.py
+++ yalemat/hello.py
@@ -1,14 +1,14 @@
 import scipy.io as sio 
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
+import os
 #https://blog.csdn.net/qq_43711697/article/details/104175500
-datafile="C:\\Users\\Administrator\\Desktop\\Courses\\yalemat\\data\\yalemat.mat"
+path=os.getcwd()
+datafile=path+"\\yalemat\\data\\yalemat.mat"
 print(datafile)
 f =sio.loadmat(datafile)
 a=np.array(32*32)
 a=f['yalemat'].T[2]
-#print(f.keys())
 a.shape=32,32
 a=a.T
 #print(a)
