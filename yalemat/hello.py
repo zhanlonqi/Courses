230ba405e9cd8a32e3b3628e31ded0c92bf406e1
zhanlonqi <1955234743@qq.com>
2020-10-09 11:23:29 +0800

    add data to ex02

--- yalemat/hello.py
+++ yalemat/hello.py
@@ -1,15 +1,17 @@
 import scipy.io as sio 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 #https://blog.csdn.net/qq_43711697/article/details/104175500
-datafile="C:\\work\\python\\hello\\Courses\\data\\yalemat.mat"
+datafile="C:\\Users\\Administrator\\Desktop\\Courses\\yalemat\\data\\yalemat.mat"
+print(datafile)
 f =sio.loadmat(datafile)
-#a=np.array(32*32)
+a=np.array(32*32)
 a=f['yalemat'].T[2]
-print(f.keys())
+#print(f.keys())
 a.shape=32,32
 a=a.T
-print(a)
+#print(a)
 plt.imshow(a,cmap="gray")
 plt.show()
 
