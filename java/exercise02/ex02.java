67cb86e60fa1b4b88e6d69f1a5255228b5e6101a
zhanlonqi <1955234743@qq.com>
2020-10-22 19:23:54 +0800

    add some pictures

--- java/exercise02/ex02.java
+++ java/exercise02/ex02.java
@@ -6,11 +6,9 @@ public class ex02
  public static void main(String args[])
  {
   int length=args.length;
-  ex02 e=new ex02();
 	Arrays.sort(args,new StringComparator());
   //通过命令行参数输入多个字符串
   //对字符串进行排序，按字母顺序降序排列
-
   for(int i=0;i<length;i++)
    System.out.println(args[i]);
  }
