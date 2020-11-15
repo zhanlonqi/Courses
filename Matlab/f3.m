7195ceeb0620ae05c81089881c5d4ef57f492331
zhanlonqi <1955234743@qq.com>
2020-11-15 13:15:34 +0800

    finish homework for matlab

---
+++ Matlab/f3.m
@@ -0,0 +1,12 @@
+function salary=f3(a)
+if a<60
+    salary=a*84-700;
+    if(salary<0)
+        salary=0;
+        disp('杨白劳？');
+elseif a>120
+    salary=a*84+(a-120)*(84*0.15);
+else
+    salary=84*a;
+end
+end
