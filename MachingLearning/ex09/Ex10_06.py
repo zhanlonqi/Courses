af1f4ed758f4fe2788724c025b580c91972a676f
zhan <zlq6forever@163.com>
2020-12-28 15:31:38 +0800

    fix some issues

--- MachingLearning/ex09/Ex10_06.py
+++ MachingLearning/ex09/Ex10_06.py
@@ -17,17 +17,19 @@ data_path =sys.path[0]+'/../data/yalemat.mat'
 # 读取 mat 文件中的数据
 data_dic = scio.loadmat(data_path)
 X = data_dic['yalemat'].T    # 样本集，每一行对应一个样本
-X_scaler = StandardScaler()
-x = X_scaler.fit_transform(X)
 
-pca = PCA(n_components=0.9)# 保证降维后的数据保持90%的信息
-pca.fit(x)
-pca.transform(x)
-print(x.shape)
+pca = PCA(n_components=0.95)# 保证降维后的数据保持90%的信息
 
+x=pca.fit_transform(X.T)
+res=pca.inverse_transform(x)
+print(res.shape)
 
-for j in range(15):
-    plt.subplot(5,3,j+1)
-    plt.imshow(x[j].reshape(32,32).T,cmap='gray')
+k=4
+for j in range(k):
+    plt.subplot(2,k,j+1)
+    plt.imshow(res.T[j].reshape(32,32).T,cmap='gray')
+    plt.subplot(2,k,j+1+k)
+    plt.imshow(X[j].reshape(32,32).T,cmap='gray')
+
 plt.show()
 #print(inv_X)
